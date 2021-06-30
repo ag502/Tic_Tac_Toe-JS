4e597016d79fcaf7054be9b9c1eb7bb2ff0de5d0
LeeJaeYun <ag502@naver.com>
2021-07-01 00:50:33 +0900

    chore : apply prettier

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 export const players = {
-  PLAYER_1: "PLAYER_1",
-  PLAYER_2: "PLAYER_2"
-}
+  PLAYER_1: 'PLAYER_1',
+  PLAYER_2: 'PLAYER_2',
+};
 
 export const calculateWinner = (board) => {
   const lines = [
@@ -12,18 +12,22 @@ export const calculateWinner = (board) => {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
-  console.log(board)
+  console.log(board);
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (board[a] !== "empty" && board[a] === board[b] && board[a] === board[c]) {
-      if (board[a] === "X") {
-        return players.PLAYER_1
-      } else if (board[a] === "O") {
-        return players.PLAYER_2
+    if (
+      board[a] !== 'empty' &&
+      board[a] === board[b] &&
+      board[a] === board[c]
+    ) {
+      if (board[a] === 'X') {
+        return players.PLAYER_1;
+      } else if (board[a] === 'O') {
+        return players.PLAYER_2;
       }
     }
   }
   return null;
-}
+};
