e990a4365be15250c60c3e81b2f24a7fc0f41ff0
LeeJaeYun <ag502@naver.com>
2021-06-30 23:01:51 +0900

    feat : add cell click event

--- src/app.js
+++ src/app.js
@@ -6,11 +6,31 @@ class App {
     this.$container = $container
     this.curTurn = players.PLAYER_1
 
+    this.state = {
+      curTurn: players.PLAYER_1
+    }
+
     this.render()
   }
 
-  handleCellClick = (e) => {
+  setState = (stateKey, newState) => {
+    this.state[stateKey] = newState
   }
+
+  handleCellClick = (e) => {
+    if (this.state.curTurn === players.PLAYER_1) {
+      const x = document.createElement("img")
+      x.setAttribute("src", "assets/x.svg")
+      e.target.appendChild(x)
+      this.setState("curTurn", players.PLAYER_2)
+    } else {
+      const o = document.createElement("img")
+      o.setAttribute("src", "assets/o.svg")
+      e.target.appendChild(o)
+      this.setState("curTurn", players.PLAYER_1)
+    }
+  }
+
 
   render() {
     new Board({container: this.$container, onClick: this.handleCellClick})
